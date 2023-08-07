68511639748df930e23562e486e7ce0908995be9
prathmesh0125 <bidveprathmesh590@gmail.com>
2023-08-07 19:44:49 +0530

    Project data

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,12 +1,69 @@
-import React from 'react'
-import "../Styles/Project.css"
+import React from "react";
+import "../Styles/Project.css";
+import { Button, Typography } from "@mui/material";
+import { AiOutlineProject } from "react-icons/ai";
+import { Delete } from "@mui/icons-material";
 
-const Project = () => {
-  return (
+const projectCard=({
+  url,
+  projectImg,
+  Projecttitle,
+  description,
+  tecnologies,
+  isAdmin=false,
+
+})=>{
+  
+  return(
+    <>
+    <a href={url} className="Projectcard" target="_blank">
+
+    
     <div>
-     My Project
+      <img src={projectImg} alt="projectImg" />
+    <Typography variant="h5">{Projecttitle}</Typography>
     </div>
+    <div>
+    <Typography variant="h4">About Project</Typography>
+    <Typography >{description}</Typography>
+    <Typography variant="h6">Tech Stack: {tecnologies}</Typography>
+      
+    </div>
+    </a>
+    {
+      isAdmin && (
+        <Button style={{color:"rgba(40,40,40,0.7)"}}>
+          <Delete/>
+        </Button>
+      )
+    }
+    </>
+
   )
 }
 
-export default Project
+const Project = () => {
+  const Projects=[1,2,3];
+  return (
+    <div className="Projects">
+      <Typography variant="h3">
+        Projects <AiOutlineProject />
+      </Typography>
+      <div className="ProjectWrapper">
+       {
+        Projects.map((project,index)=>{
+          <projectCard  
+          url="https://github.com/prathmesh0125"
+          projectImg="Project/Barebeat.png"
+          Projecttitle="BareBeat"
+          description="djfhfsjd dsjsdnfjsdsmd ssdnsdnjsd"
+          tecnologies="Reactjs,NodeJs,Html,Css,Javascript"
+          />
+        })
+       }
+      </div>
+    </div>
+  );
+};
+
+export default Project;
